669b003f52b38890ec43e500133ccd77fab27dfc
ASmith-eng <alexsmith194@outlook.com>
2026-02-13 17:32:52 +0000

    feat: add data schema for better-auth integration

--- lib/db/relations.ts
+++ lib/db/relations.ts
@@ -1,3 +1,21 @@
 import { relations } from "drizzle-orm/relations";
-import {  } from "./schema";
+import { users, account, userSessions } from "./schema";
 
+export const accountRelations = relations(account, ({ one }) => ({
+  user: one(users, {
+    fields: [account.userId],
+    references: [users.id],
+  }),
+}));
+
+export const usersRelations = relations(users, ({ many }) => ({
+  accounts: many(account),
+  userSessions: many(userSessions),
+}));
+
+export const userSessionsRelations = relations(userSessions, ({ one }) => ({
+  user: one(users, {
+    fields: [userSessions.userId],
+    references: [users.id],
+  }),
+}));
--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -4,6 +4,9 @@ import {
   varchar,
   boolean,
   timestamp,
+  text,
+  index,
+  foreignKey,
 } from "drizzle-orm/pg-core";
 import { sql } from "drizzle-orm";
 
@@ -19,4 +22,92 @@ export const users = pgTable("users", {
   updatedAt: timestamp("updated_at", { withTimezone: true, mode: "string" })
     .defaultNow()
     .notNull(),
+  image: text(),
 });
+
+export const accounts = pgTable(
+  "accounts",
+  {
+    id: uuid().defaultRandom().notNull(),
+    account_id: text().notNull(),
+    provider_id: text().notNull(),
+    user_id: uuid().notNull(),
+    access_token: text(),
+    refresh_token: text(),
+    id_token: text(),
+    access_token_expires_at: timestamp({ withTimezone: true, mode: "string" }),
+    refresh_token_expires_at: timestamp({ withTimezone: true, mode: "string" }),
+    scope: text(),
+    password: text(),
+    created_at: timestamp({ withTimezone: true, mode: "string" })
+      .defaultNow()
+      .notNull(),
+    updated_at: timestamp({ withTimezone: true, mode: "string" }).notNull(),
+  },
+  (table) => [
+    index("account_user_id_idx").using(
+      "btree",
+      table.user_id.asc().nullsLast().op("uuid_ops"),
+    ),
+    foreignKey({
+      columns: [table.user_id],
+      foreignColumns: [users.id],
+      name: "account_user_id_fkey",
+    }).onDelete("cascade"),
+  ],
+);
+
+export const userSessions = pgTable(
+  "user_sessions",
+  {
+    id: uuid().defaultRandom().notNull(),
+    expiresAt: timestamp("expires_at", {
+      withTimezone: true,
+      mode: "string",
+    }).notNull(),
+    token: text().notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true, mode: "string" })
+      .defaultNow()
+      .notNull(),
+    updatedAt: timestamp("updated_at", {
+      withTimezone: true,
+      mode: "string",
+    }).notNull(),
+    ipAddress: varchar("ip_address", { length: 40 }),
+    userAgent: text("user_agent"),
+    userId: uuid("user_id").notNull(),
+  },
+  (table) => [
+    index("user_sessions_user_id_idx").using(
+      "btree",
+      table.userId.asc().nullsLast().op("uuid_ops"),
+    ),
+    foreignKey({
+      columns: [table.userId],
+      foreignColumns: [users.id],
+      name: "user_sessions_user_id_fkey",
+    }).onDelete("cascade"),
+  ],
+);
+
+export const verification = pgTable(
+  "verification",
+  {
+    id: uuid().defaultRandom().notNull(),
+    identifier: text().notNull(),
+    value: text().notNull(),
+    expiresAt: timestamp({ withTimezone: true, mode: "string" }).notNull(),
+    createdAt: timestamp({ withTimezone: true, mode: "string" })
+      .defaultNow()
+      .notNull(),
+    updatedAt: timestamp({ withTimezone: true, mode: "string" })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+  },
+  (table) => [
+    index("verification_identifier_idx").using(
+      "btree",
+      table.identifier.asc().nullsLast().op("text_ops"),
+    ),
+  ],
+);

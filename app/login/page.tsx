b03710c569aa105bb5db544b6ec1fa2bc96072b8
ASmith-eng <alexsmith194@outlook.com>
2026-02-18 00:00:44 +0000

    feat: install react-hook-form + shadcn + wip login form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,72 @@
+"use client";
+
+import { useForm, Controller } from "react-hook-form";
+import {
+  Field,
+  FieldContent,
+  FieldDescription,
+  FieldError,
+  FieldLabel,
+  FieldLegend,
+  FieldSeparator,
+  FieldSet,
+  FieldGroup,
+  FieldTitle,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 export default function Login() {
-  return <div>Login page</div>;
+  const { register, handleSubmit, control } = useForm({
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  return (
+    <main>
+      <div>Login page</div>
+
+      <form
+        id="login-form"
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        <FieldSet>
+          <FieldLegend>Login</FieldLegend>
+          <FieldDescription>Description</FieldDescription>
+          <FieldGroup>
+            <Controller
+              name="email"
+              control={control}
+              rules={{
+                required: "Your email is required",
+                pattern: {
+                  value: /^[^@\s]+@[^@.\s]+\.[a-zA-Z0-9\-]+$/,
+                  message: "Please enter a valid email address",
+                },
+              }}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel htmlFor="email">Email</FieldLabel>
+                  <Input
+                    {...field}
+                    id="email"
+                    aria-invalid={fieldState.invalid}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+        </FieldSet>
+      </form>
+      <Button type="submit" form="login-form">
+        Submit
+      </Button>
+    </main>
+  );
 }
